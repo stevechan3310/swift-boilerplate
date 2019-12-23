4b87eed61f3ada753ee754b04b04a8548d3f6b6d
stevechan3310 <stevechan8820@ymail.com>
2019-12-23 11:51:05 +0800

    Initial project setup with pod configured

---
+++ swift-boilerplate/swift-boilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-boilerplate
+//
+//  Created by Steve Chan on 12/19/19.
+//  Copyright © 2019 COMPANY_NAME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift-boilerplate/swift-boilerplateTests/swift_boilerplateTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_boilerplateTests.swift
+//  swift-boilerplateTests
+//
+//  Created by Steve Chan on 12/19/19.
+//  Copyright © 2019 COMPANY_NAME. All rights reserved.
+//
+
+import XCTest
+@testable import swift_boilerplate
+
+class swift_boilerplateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
